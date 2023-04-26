43db27aa120d20c754ffcd02f43a4d431c195169
Shafigullin Ilyas <Ilyas.Shafigullin.05@mail.ru>
2023-04-26 12:53:55 +0300

    Добавили код

---
+++ CodProgrammy/CodProgrammy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodProgrammy
+{
+    class Program
+    {
+        static int SumOfDigits(int number) //Метод для вычисления суммы цифр числа
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+        static void Main(string[] args) //Главный метод программы
+        {
+            int n, maxNumber = 0, maxSum = 0;
+            Console.Write("Введите количество чисел: ");
+            n = int.Parse(Console.ReadLine()); //Ввод количества чисел
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите натуральное число: ");
+                int number = int.Parse(Console.ReadLine()); //Ввод числа
+                int sumOfDigits = SumOfDigits(number); //Вычисление суммы цифр числа
+                if (sumOfDigits > maxSum) //Проверка на максимальность суммы цифр
+                {
+                    maxSum = sumOfDigits;
+                    maxNumber = number;
+                }
+            }
+            Console.WriteLine($"Число с наибольшей суммой цифр: {maxNumber}, сумма цифр: {maxSum}");
+            Console.ReadLine(); //Чтобы окно консоли не закрылось сразу
+        }
+    }
+}
+
